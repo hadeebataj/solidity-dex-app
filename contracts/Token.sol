1997b8814e63099f56937c61f404a4a60e37e4d7
hadeebataj <hadeebataj@gmail.com>
2024-02-15 17:23:07 +0000

    7. Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -39,17 +39,22 @@ contract Token {
     ) public returns (bool success) {
         // Require that sender has enough tokens to spend
         require(balanceOf[msg.sender] >= _value, "Not enough tokens to spend");
+
+        _transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
         require(_to != address(0));
 
         //Deduct tokens from spender
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_from] = balanceOf[_from] - _value;
         //Credit tokens from receiver
         balanceOf[_to] = balanceOf[_to] + _value;
 
         // Emit Event
-        emit Transfer(msg.sender, _to, _value);
-
-        return true;
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(
@@ -60,6 +65,23 @@ contract Token {
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // check approval
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender], "Not enough allowance");
+
+        // reset the allowance
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+        // spend tokens
+        _transfer(_from, _to, _value);
         return true;
     }
 }
