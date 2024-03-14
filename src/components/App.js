c9e185b61c3204cbfbb9f6a062a65b02c95e290c
hadeebataj <hadeebataj@gmail.com>
2024-03-14 11:37:33 +0000

    - My Orders module complete

--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,7 @@ import Order from './Order';
 import OrderBook from './OrderBook';
 import PriceChart from './PriceChart';
 import Trades from './Trades';
+import Transactions from './Transactions';
 
 function App() {
   const dispatch = useDispatch();
@@ -81,7 +82,7 @@ function App() {
         <section className="exchange__section--right grid">
           <PriceChart />
 
-          {/* Transactions */}
+          <Transactions />
 
           <Trades />
 
